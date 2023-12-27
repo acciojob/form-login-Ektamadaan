4f2bea4d5e33383248c9dee3b8fa0db0c19e58c6
Ektamadaan <ektamadaan45@gmail.com>
2023-12-27 21:37:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function getFormvalue() {
     //Write your code here
-
+	let input1 = document.getElementsByName("fname");
+	let input2 = document.getElementsByName("lname");
+	
+	document.addEventListener("click" , function() {
+		let i1 = input1.value;
+		let i2 = input2.value;
+		alert(i1 + " " + i2);
+	})
 }
